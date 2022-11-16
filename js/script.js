d38149df7a93e21a2f595e83188edf6d6e8a5e0f
Abdulvahobjon <abdulvahob0714@gmail.com>
2022-11-17 03:03:31 +0500

    oneproject end

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
-const tabheaderItem = document.querySelectorAll('.tabheader__item'),
-      tabheaderItems = document.querySelector('.tabheader__items'),
-      tabcontent = document.querySelector('.tabcontent');
+window.addEventListener("DOMContentLoaded", function () {
+  const tabheaderItem = document.querySelectorAll(".tabheader__item"),
+    tabheaderItems = document.querySelector(".tabheader__items"),
+    tabcontent = document.querySelectorAll(".tabcontent");
+
+  function removeClass() {
+    tabheaderItem.forEach((item, index) => {
+      item.classList.remove("tabheader__item_active");
+      tabcontent[index].classList.add("hide");
+    });
+  }
+
+  function addClass(index) {
+    tabcontent[index].classList.remove("hide");
+    tabheaderItem[index].classList.add("tabheader__item_active");
+  }
+
+  removeClass();
+  addClass(0);
+
+  tabheaderItem.forEach((item, index) => {
+    item.addEventListener("click", function () {
+      removeClass();
+      addClass(index);
+    });
+  });
+});
