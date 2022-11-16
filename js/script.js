1ccac6e6c6facacdac10232f297fccd57d6a5b38
Abdulvahobjon <abdulvahob0714@gmail.com>
2022-11-17 02:47:22 +0500

    start project

---
+++ js/script.js
@@ -0,0 +1,3 @@
+const tabheaderItem = document.querySelectorAll('.tabheader__item'),
+      tabheaderItems = document.querySelector('.tabheader__items'),
+      tabcontent = document.querySelector('.tabcontent');
